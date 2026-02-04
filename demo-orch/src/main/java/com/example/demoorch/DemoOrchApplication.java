57cded4a7293fe4d1044a2fabd9c6f21398fc24e
Jalina2007 <jalinajayasinghe7@gmail.com>
2026-02-04 16:03:00 +0530

    Init commit

---
+++ demo-orch/src/main/java/com/example/demoorch/DemoOrchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoorch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOrchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOrchApplication.class, args);
+    }
+
+}
